4e6a7dff7a4650d2276d59e966f1e0d5a60c2aa5
Jan-Sebastian Fabík <honza@fabik.org>
2017-06-06 00:24:22 +0200

    api: created project

---
+++ api/src/main/java/org/fabik/macrodataapi/MacrodataapiApplication.java
@@ -0,0 +1,12 @@
+package org.fabik.macrodataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrodataapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrodataapiApplication.class, args);
+	}
+}
